7c5399ced51a34dd936ebb1543be5ca764f0abe9
schlixmann <felixschumann95@gmx.de>
2023-06-12 13:19:34 +0200

    new_rest

--- rest_api.py
+++ rest_api.py
@@ -5,8 +5,8 @@ logging.info('Started')
 from gevent import monkey; monkey.patch_all()
 import threading
 import bottle
-from bottle import request, response
-from bottle import post, get, put, delete
+from bottle import request, response, static_file
+from bottle import post, get, put, delete, route
 from tree_allocation.helpers import *
 import json
 import requests
@@ -26,18 +26,18 @@ def get_info():
 @get("/resources")
 def get_resources():
     '''Return the resource xml'''
-    resource_xml = open("./config/res_config_cost.xml").read()
-    response.headers['Content-Type'] = 'text/xml'
+    resource_xml = open("./config/res_config_cost2.xml").read()
+    response.headers['Content-Type'] = 'text/xml'  
     return resource_xml
 
 @post("/allocation/")
 def update_process():
+    # Get measure and Operator from form
     try:
         measure = request.forms.get("measure").lower().strip()
     except:
         measure = None
 
-     
     try: 
         operator = request.forms.get("operator")
         match operator:
@@ -48,11 +48,12 @@ def update_process():
     except:
         operator = None
 
+    # Get URLs from Header
     print("CPEE-Instance URL: ", request.headers.raw("Cpee-Instance-Url"))
     instance_url = request.headers.raw("Cpee-Instance-Url")
     description_url = instance_url + "properties/description/"
 
-    
+    # Get Resource URL from form
     if request.forms.get("resource_url"):
         resource_url = request.forms.get("resource_url")
         print("Resource config File URL", resource_url)
@@ -60,11 +61,13 @@ def update_process():
     else: 
         manipulated_process_model = allocate_process(description_url, measure=measure, operator=operator)
 
-    
+    # Start allocation task
     event = threading.Event()
     t1 = threading.Thread(target=my_request, args=(instance_url, manipulated_process_model))
     t1.daemon = True
     t1.start()
+
+    # Send response 400 to stop CPEE
     print("returning")
     response.status = 400
     print(response.status_line)
@@ -109,6 +112,24 @@ def my_request(instance_url, xml_str):
     response = requests.request("PUT", status_url, data=payload, headers=headers)
     print(response.status_code)
     logging.info(f"PUT state to running with status code: {response.status_code}")
+
+
+# Try to generate an Editor for resource specification file
+@post("/edit/add_resprofile/")
+def edit_resources():
+    print(request.body)
+    """Return HTML to edit resources"""
+    response.status = 201
+    return response
+
+
+@route("/edit/resources/")
+def edit_resources():
+    """Return HTML to edit resources"""
+
+    return static_file("res_editor.html", root="frontend/")
+
+
 
 if __name__ == '__main__':
     bottle.run(host = '127.0.0.1', port = 8000, server="gevent")
