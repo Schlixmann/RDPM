072b89fe2de07cefd874a8905f3bef058f4a5048
schlixmann <felixschumann95@gmx.de>
2023-06-22 13:37:42 +0200

    fixed allocation to allocaiton package

--- tree_allocation/allocation/allocation.py
+++ tree_allocation/allocation/allocation.py
@@ -1,9 +1,14 @@
 from tree_allocation.tree import task_node as tn
 from tree_allocation.tree import res_node as rn
+from tree_allocation.helpers import get_all_resources, get_all_tasks, get_process_model
+from tree_allocation.allocation.change_operations import *
 
+from PrettyPrint import PrettyPrintTree
 from proc_resource import *
 from lxml import etree
 import warnings
+import logging
+logger = logging.getLogger(__name__)
 
 ns = {"cpee2": "http://cpee.org/ns/properties/2.0", 
         "cpee1":"http://cpee.org/ns/description/1.0"}
@@ -70,6 +75,72 @@ def build_allo_tree(root, av_resources:Resource=[], excluded=[], task_parent=Non
                     resource.add_child(build_allo_tree(task, av_resources, excluded=ex_branch, task_parent=root, res_parent=resource))
                 
     return root
+
+def allocate_process(cpee_url, resource_url="http://127.0.0.1:8000/resources", measure=None, operator=min):
+    #resources = get_all_resources("./config/res_config_5.xml")
+    resources = get_all_resources(resource_url)
+    logger.info(f"Test logging from module {[r.name for r in resources]}")
+    process_model_xml, process_model_etree = get_process_model(cpee_url)
+    tasklabels = []
+    for task in get_all_tasks(cpee_url):
+        try:
+            tasklabels.append({"task_id": task.attrib["id"] ,"label": task.attrib["label"]})
+            
+        except:
+            try:
+                attrib = task.find(".//cpee1:parameters", ns)
+                if not attrib.find(".//cpee1:label", ns).text:
+                     raise Exception("Task {} has no label.".format(task.attrib["id"]))
+                else:
+                     tasklabels.append({"task_id": task.attrib["id"], "label": attrib.find(".//cpee1:label", ns).text })
+            except Exception as e:
+                print(e)
+                continue
+    print("Tasklabels: ", tasklabels)
+    print("Resource: ", resources)
+
+    
+    
+    i = 0
+    for task in tasklabels:
+        print(f"Start Allocation of {task}")
+        root = tn.TaskNode(task["task_id"], task["label"])
+        try:
+            build_allo_tree(root, resources)
+            #print_allo_tree(root)
+
+            pt = PrettyPrintTree(lambda x: x.children, lambda x: "task:" + str(x.label) + " " + str(x.id) if type(x) == tn.TaskNode else "res:" + str(x.name) + " rp:" + str(x.resource_profile.name) + f"\n {str(measure)}: "  + str(x.measure[measure]))
+            pt(root)
+
+            
+            best_branch, branch_measure, value, best_node = root.get_best_branch(measure=measure, operator=operator)
+            print("Best_branch", f"All branch measures: {branch_measure}, best measure: {value}")
+            pt(best_node)
+            # TODO: create change operation
+
+            all_resources = best_node.get_all_nodes()
+            print([resource.get_name for resource in all_resources])
+
+            process_model_xml, changed_model_etree = create_change_operation(best_node, process_model_xml)
+            
+            with open(f"./output/out_{str(i)}.xml", "wb") as f:
+                f.write((process_model_xml))
+
+            logger.info(f"Task successfully allocate: {root.get_name}")
+        except ResourceError as e:
+            e.add_note(f"No allocation possible, the task: {task} is skipped")
+            print("Task not allocatable:", e.task.get_name, ", Message:", e.message)
+            
+        #except Exception as e:
+        #    print("Allocation Failed")
+        #    print(e)
+            
+        i += 1
+        
+        
+    with open("./output/test.xml", "wb") as f:
+        f.write((process_model_xml))
+    return process_model_xml
 
 class ResourceError(Exception):
     # Exception is raised if no sufficiant allocation for a task can be found for available resources
